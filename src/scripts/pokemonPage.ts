b59472295b19cad04d987c2657287dcbcf1d5143
ori amram <oriamram111@gmail.com>
2022-07-03 00:35:42 +0300

    added minidex

--- src/scripts/pokemonPage.ts
+++ src/scripts/pokemonPage.ts
@@ -5,13 +5,19 @@ const urlParams = new URLSearchParams(window.location.search);
 const URLid = +urlParams.get("id");
 const pokedex = document.getElementsByClassName("pokedexContainer")[0] as HTMLElement;
 const statsElement = document.querySelectorAll(".stats");
+const pokedexImg = document.getElementsByClassName("pokedex")[0] as HTMLElement;
+const miniDex = document.createElement("img");
+miniDex.setAttribute("alt", "Pini Pokedex");
+miniDex.setAttribute("src", "./images/miniDex.jpg");
+miniDex.classList.add("pokedex");
 const typeContainer = document.getElementsByClassName("types")[0] as HTMLElement;
 const nextPage = document.querySelector("#nextPage") as HTMLElement;
 pokedex.addEventListener("click", (e) => {
 	let element = e.target as HTMLElement;
 	element.id === nextPage.id ? (window.location.href = `/pokemon.html?id=${POKEMON.data.id + 1}`) : null;
 });
-
+window.onresize = onResize;
+window.onload = onResize;
 const prevPage = document.getElementById("prevPage");
 const pokemons: Pokemon[] = Utility.getPokemonsFromLocalStorage();
 const POKEMON = pokemons.find((pokemon) => pokemon.data.id === URLid);
@@ -37,3 +43,16 @@ function addStats(): void {
 }
 
 pokedex.innerHTML += `<img src="${POKEMON.data.img}" alt="pokemon" class="pokemonImg" />`;
+let done: boolean = false;
+
+function onResize() {
+	if (window.innerWidth <= 600 && !done) {
+		document.getElementsByClassName("pokedex")[0].remove();
+		pokedex.appendChild(miniDex);
+		done = true;
+	} else if (window.innerWidth > 600 && done) {
+		document.getElementsByClassName("pokedex")[0].remove();
+		pokedex.appendChild(pokedexImg);
+		done = false;
+	}
+}
