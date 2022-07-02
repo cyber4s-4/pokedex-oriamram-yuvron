e050c5801676dac87ed05252b8c718ac51e6a841
yuvron <yuvr99@gmail.com>
2022-07-02 14:43:17 +0300

    eslint fix

--- src/scripts/pokemonPage.ts
+++ src/scripts/pokemonPage.ts
@@ -1,18 +1,17 @@
+import { Utility } from "./utility";
 import { Pokemon, PokemonData, PokemonSpecs } from "./Pokemon";
+
 const urlParams = new URLSearchParams(window.location.search);
 const URLid = +urlParams.get("id");
 const pokedex = document.getElementsByClassName("pokedexContainer")[0] as HTMLElement;
 const statsElement = document.querySelectorAll(".stats");
 const typeContainer = document.getElementsByClassName("types")[0] as HTMLElement;
 
-let pokemons: Pokemon[] = [];
-getFromLocalStorage();
-let POKEMON: Pokemon;
-pokemons.forEach((pokemon) => {
-	if (pokemon.data.id === URLid) POKEMON = pokemon;
-});
+const pokemons: Pokemon[] = Utility.getPokemonsFromLocalStorage();
+const POKEMON = pokemons.find((pokemon) => pokemon.data.id === URLid);
+
 addStats();
-function addStats() {
+function addStats(): void {
 	statsElement.forEach((element) => {
 		let starter;
 		element.id === "name" || element.id === "id" ? (starter = POKEMON.data) : (starter = POKEMON.data.specs);
@@ -27,12 +26,3 @@ function addStats() {
 	});
 }
 pokedex.innerHTML += `<img src="${POKEMON.data.img}" alt="pokemon" class="pokemonImg" />`;
-
-function addToLocalStorage(): void {
-	localStorage.setItem("pokemons", JSON.stringify(pokemons.map((pokemon) => pokemon.data)));
-}
-// Getting pokemons from local storage and pushing them to the local data
-export function getFromLocalStorage(): void {
-	const storagedData = JSON.parse(localStorage.getItem("pokemons"));
-	pokemons = storagedData.map((pokemonData) => new Pokemon(pokemonData));
-}
