8d3834d67542a180d480dd465471c62e39f0321d
yuvron <yuvr99@gmail.com>
2022-07-11 14:33:39 +0300

    gulp bug fix

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,7 @@ gulp.task("default", gulp.series("clean", "images", "ts-server", "build-ts-clien
 
 // Heroku clean files
 gulp.task("heroku-clean", () => {
-	return gulp.src(["deploy/package.json", "deploy/package-lock.json", "deploy/dist"]).pipe(clean());
+	return gulp.src(["deploy/package.json", "deploy/package-lock.json", "deploy/dist"], { allowEmpty: true }).pipe(clean());
 });
 
 // Heroku copy root files
