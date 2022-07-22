60b0d6d445314dcc33b4572dde1e6cd93fbcc3f3
student <oriamram@cyber4s.dev>
2022-07-22 16:05:11 +0300

    trying to add json to sql select

---
+++ data/database.js
@@ -0,0 +1,32 @@
+const { MongoManager } = require("../dist/server/mongo.js");
+const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv").config();
+const db = new MongoManager();
+async function addAllPokemons() {
+	console.log("a");
+	const allPokemonsPath = path.join(__dirname, "allPokemons.json");
+	const allPokemons = JSON.parse(fs.readFileSync(allPokemonsPath, "utf8")).slice(0, 8000);
+	let sql = `INSERT INTO pokemons (name, id, img, specs) VALUES `;
+	for (let i = 0; i < allPokemons.length; i++) {
+		const offset = i * 4;
+		sql += `($${offset + 1},$${offset + 2},$${offset + 3},$${offset + 4}),`;
+	}
+	sql = sql.slice(0, -1);
+	const pokemonValues = allPokemons.map((pokemon) => Object.values(pokemon));
+	// await db.client.query(sql, pokemonValues.flat());
+	console.log("SQL: INSERT INTO pokemons");
+	// console.log(await db.getPokemonById(2));
+	console.log((await db.client.query(`SELECT * FROM pokemons WHERE tags::jsonb = '[ "grass", "poison" ]'::jsonb`)).rows);
+}
+db.init()
+	.then(() =>
+		addAllPokemons().then(() => {
+			console.log("kaka");
+			db.client.end();
+		})
+	)
+	.catch((err) => {
+		console.log(err.message);
+		db.client.end();
+	});
--- src/server/mongo.ts
+++ src/server/mongo.ts
@@ -10,18 +10,12 @@ export class MongoManager {
 	// usersCollection: Collection;
 
 	constructor() {
-		// this.client = new MongoClient(process.env.mongoUrl);
 		this.client = new Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 	}
 
 	async init(): Promise<void> {
 		await this.client.connect();
 		await this.createTable("pokemons");
-		// console.log("Database connected");
-		// this.db = this.client.db("pokedex");
-		// this.pokemonsCollection = this.db.collection("pokemons");
-		// this.usersCollection = this.db.collection("users");
-		// if ((await this.pokemonsCollection.countDocuments()) === 0) await this.addAllPokemons();
 	}
 
 	async createTable(name: string) {
@@ -33,71 +27,63 @@ export class MongoManager {
 		)`);
 	}
 
-	async insertOne() {
-		await this.client.query(`INSERT INTO pokemons(id,name,img,specs) VALUES(1,'yuvalkaka','aaaaa',"{'wallak':['me','andyou'],'height':'123'}")`);
+	async getPokemonsByFilter(
+		token: string,
+		searchTerm: string,
+		types: String[],
+		combinedTypes: boolean,
+		sortType: string,
+		sortDirection: number,
+		start: number
+	): Promise<any[]> {
+		// const findObject = {
+		// 	$or: [{ name: new RegExp(`.*${searchTerm}.*`, "i") }, { id: +searchTerm }],
+		// };
+		let sql = `SELECT * FROM pokemons WHERE (name LIKE '%${searchTerm}%' OR id = ${+searchTerm}) `;
+		if (types.length > 0) {
+			if (combinedTypes) {
+				// findObject["specs.types"] = { $all: types };
+				sql += `AND `;
+			} else {
+				findObject["specs.types"] = { $in: types };
+			}
+		}
+		const matchingPokemons = await this.pokemonsCollection
+			.find(findObject)
+			// .sort({
+			// 	[sortType]: sortDirection,
+			// 	_id: 1,
+			// })
+			.skip(start)
+			.limit(100)
+			.toArray();
+		if (start === 0) {
+			const filterObject = { searchTerm, types, combinedTypes };
+			const favorites = await this.getUserFavoritePokemons(token, filterObject);
+			favorites.sort((a, b) => (a[sortType] > b[sortType] ? sortDirection * -1 : sortDirection));
+			for (const favorite of favorites) {
+				matchingPokemons.forEach((matchingPokemon, index) => {
+					if (favorite.id === matchingPokemon.id) {
+						matchingPokemons.splice(index, 1);
+					}
+				});
+				matchingPokemons.unshift(favorite);
+			}
+		}
+		matchingPokemons.forEach((pokemon) => delete pokemon["_id"]);
+		return matchingPokemons;
 	}
-	// 	async addAllPokemons(): Promise<void> {
-	// 		this.removeAllPokemons();
-	// 		const allPokemonsPath = path.join(__dirname, "../../data/allPokemons.json");
-	// 		const allPokemons = JSON.parse(fs.readFileSync(allPokemonsPath, "utf8"));
-	// 		await this.pokemonsCollection.insertMany(allPokemons);
-	// 	}
 
-	// 	async removeAllPokemons(): Promise<void> {
-	// 		await this.pokemonsCollection.deleteMany({});
-	// 	}
+	async getPokemonById(id: number): Promise<any> {
+		return (await this.client.query(`SELECT * FROM pokemons WHERE id = ${id}`)).rows[0];
+	}
 
-	// 	async getAllPokemons(): Promise<any[]> {
-	// 		return await this.pokemonsCollection.find({}).toArray();
-	// 	}
-
-	// async getPokemonsByFilter(token: string, searchTerm: string, types: String[], combinedTypes: boolean, sortType: string, sortDirection: number, start: number): Promise<any[]> {
-	// 	const findObject = {
-	// 		$or: [{ name: new RegExp(`.*${searchTerm}.*`, "i") }, { id: +searchTerm }],
-	// 	};
-	// 	if (types.length > 0) {
-	// 		if (combinedTypes) {
-	// 			findObject["specs.types"] = { $all: types };
-	// 		} else {
-	// 			findObject["specs.types"] = { $in: types };
-	// 		}
-	// 	}
-	// 	const matchingPokemons = await this.pokemonsCollection
-	// 		.find(findObject)
-	// 		// .sort({
-	// 		// 	[sortType]: sortDirection,
-	// 		// 	_id: 1,
-	// 		// })
-	// 		.skip(start)
-	// 		.limit(100)
-	// 		.toArray();
-	// 	if (start === 0) {
-	// 		const filterObject = { searchTerm, types, combinedTypes };
-	// 		const favorites = await this.getUserFavoritePokemons(token, filterObject);
-	// 		favorites.sort((a, b) => (a[sortType] > b[sortType] ? sortDirection * -1 : sortDirection));
-	// 		for (const favorite of favorites) {
-	// 			matchingPokemons.forEach((matchingPokemon, index) => {
-	// 				if (favorite.id === matchingPokemon.id) {
-	// 					matchingPokemons.splice(index, 1);
-	// 				}
-	// 			});
-	// 			matchingPokemons.unshift(favorite);
-	// 		}
-	// 	}
-	// 	matchingPokemons.forEach((pokemon) => delete pokemon["_id"]);
-	// 	return matchingPokemons;
+	// async createUser(): Promise<string> {
+	// 	const insertedDocument = await this.usersCollection.insertOne({
+	// 		favoritePokemons: [],
+	// 	});
+	// 	return insertedDocument.insertedId.toString();
 	// }
-
-	// 	async getPokemonById(id: number): Promise<any> {
-	// 		return await this.pokemonsCollection.findOne({ id: id });
-	// 	}
-
-	// 	async createUser(): Promise<string> {
-	// 		const insertedDocument = await this.usersCollection.insertOne({
-	// 			favoritePokemons: [],
-	// 		});
-	// 		return insertedDocument.insertedId.toString();
-	// 	}
 
 	// 	async getUserFavoritePokemons(token: string, filterObject = { searchTerm: "", types: [], combinedTypes: false }): Promise<any[]> {
 	// 		const user = await this.usersCollection.findOne({
